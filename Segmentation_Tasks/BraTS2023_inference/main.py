38199b21c1e895e6e740ef931ccdae051eacf5d1
Daryl Wilding-McBride <daryl.wilding-mcbride@rmit.edu.au>
2025-04-30 15:36:53 +1000

    Update main.py

--- Segmentation_Tasks/BraTS2023_inference/main.py
+++ Segmentation_Tasks/BraTS2023_inference/main.py
@@ -18,7 +18,7 @@ def infer(
     # First step - Convert dataset from BraTS2023 to nnUNet format
     print("Doing first step")
     input_folder_nnunet = 'converted_dataset/'
-    if output_path is None:
+    if data_path is None:
         print('not converting the raw data - using the contents of {}'.format(input_folder_nnunet))
     else:
         print('converting the raw data from {}'.format(data_path))
@@ -66,10 +66,8 @@ def infer(
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Segmentation inference")
-    parser.add_argument("--data_path", type=str, default=None, required=False, help="Path with the raw cases, following the BraTS 2023 Glioma challenge")
+    parser.add_argument("--data_path", type=str, required=False, help="Path with the raw cases, following the BraTS 2023 Glioma challenge")
     parser.add_argument("--output_path", type=str, required=True, help="Path to save the predictions")
     parser.add_argument("--nnUNet_results", type=str, required=True, help="Path to the results of the nnUNet training")
     args = parser.parse_args()
     infer(data_path=args.data_path,output_path=args.output_path, nnUNet_results=args.nnUNet_results)
-        
-
